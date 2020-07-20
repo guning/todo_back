59fe8a3af45ec35120c20c71881328914a06561c
guning <guningh@gmail.com>
2020-07-20 17:07:59 +0800

    add config loader

---
+++ config/config.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"log"
+	"strings"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+)
+
+type Config struct {}
+
+func Init(cfgFileName string) error {
+	c := Config{}
+
+	if err := c.initConfig(cfgFileName); err != nil {
+		return err
+	}
+
+	c.watchConfig()
+
+	return nil
+}
+
+func (c *Config) initConfig(cfgFileName string) error {
+	if cfgFileName != "" {
+		viper.SetConfigFile(cfgFileName)
+	} else {
+		viper.AddConfigPath("conf")
+		viper.SetConfigName("config")
+	}
+	viper.SetConfigType("yaml")
+	viper.AutomaticEnv()
+	viper.SetEnvPrefix("APISERVER")
+	replacer := strings.NewReplacer(".", "_")
+	viper.SetEnvKeyReplacer(replacer)
+	if err := viper.ReadInConfig(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Config) watchConfig() {
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		log.Printf("Config file changed: %s", e.Name)
+	})
+}
